ed49bc822bf1f031326900dbcadede1de0b000fb
Matteo Calderaro <matteocalderaro@gmail.com>
2022-11-16 10:26:52 +0100

    unicafe revisited step1 - step2

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+
+const store = createStore(reducer)
+
+const App = () => {
+  
+  const handleClick = (param) => {
+    store.dispatch({
+      type: param
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={()=>handleClick('GOOD')}>good</button>
+      <button onClick={()=>handleClick('OK')}>ok</button>
+      <button onClick={()=>handleClick('BAD')}>bad</button>
+      <button onClick={()=>handleClick('ZERO')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(storeNow)
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
+
